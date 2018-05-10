f592b25734cd29f8fe89bfc0c6c3179d44c1d162
vernipoltos14 <vladislav-p101@yandex.ru>
2018-05-10 23:33:34 +0500

    Добавление тестов студентом Портновым

--- src/test/java/Base/RabTest.java
+++ src/test/java/Base/RabTest.java
@@ -42,28 +42,5 @@ class RabTest {
         assertEquals(158704.0, testingClass.Summa(1, 145600, 12, 9));
     }
     
-    @Test
-    void summa6() {
-        assertEquals(316227.66148680047, testingClass.Summa(0, 145600, 12, 9));
-    }
-    
-    @Test
-    void summa7() {
-        assertEquals(70010.78390953124, testingClass.Summa(0, 65490, 9, 3));
-    }
-    
-    @Test
-    void summa8() {
-        assertEquals(66963.525, testingClass.Summa(1, 65490, 9, 3));
-    }
-    
-    @Test
-    void summa9() {
-        assertEquals(9846875.0, testingClass.Summa(1, 9453000, 10, 5));
-    }
-    
-    @Test
-    void summa10() {
-        assertEquals(1.1593471338719502E7, testingClass.Summa(0, 9453000, 10, 5));
-    }
+
 }
